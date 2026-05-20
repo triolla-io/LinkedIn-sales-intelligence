c939a9faba881e3358c049223cce6db4ca9fd067
Ariel Lunenfeld <ariellunenfeld@Ariels-MacBook-Pro.local>
2026-05-20 16:42:23 +0300

    chore: remove deleted LinkedIn functions from Inngest serve

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { syncFull } from "@/inngest/functions/sync-full";
-import { syncDelta } from "@/inngest/functions/sync-delta";
-import { syncCron } from "@/inngest/functions/sync-cron";
 import { enrichContact } from "@/inngest/functions/enrich-contact";
-import { sendMessage } from "@/inngest/functions/send-message";
-import { profileEnrich } from "@/inngest/functions/profile-enrich";
-import { enrichProfiles } from "@/inngest/functions/enrich-profiles";
 import { enrichCompanies } from "@/inngest/functions/enrich-companies";
 import { enrichCompaniesWeb } from "@/inngest/functions/enrich-companies-web";
 import { campaignStart } from "@/inngest/functions/campaign-start";
@@ -15,5 +9,5 @@ import { campaignFinalize } from "@/inngest/functions/campaign-finalize";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncFull, syncDelta, syncCron, enrichContact, sendMessage, profileEnrich, enrichProfiles, enrichCompanies, enrichCompaniesWeb, campaignStart, campaignSendOne, campaignFinalize],
+  functions: [enrichContact, enrichCompanies, enrichCompaniesWeb, campaignStart, campaignSendOne, campaignFinalize],
 });
